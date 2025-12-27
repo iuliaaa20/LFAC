c7ed81fdc6fd9716cb02034b5b42a9faef5c8721
Darie Iulia <iuliadarie25@gmail.com>
2025-12-28 00:26:24 +0200

    Update SymTable.h

--- tema/SymTable.h
+++ tema/SymTable.h
@@ -1,4 +1,5 @@
 #pragma once
+//Aceasta clasa este pentru a retine si lucra cu simboluri(id-uri, nume de clase, de functii)
 
 #include <iostream>
 #include <map>
@@ -6,23 +7,23 @@
 #include <fstream>
 #include <vector>
 using namespace std;
-
+//aici avem niste nume definite de noi pentru tipurile de date
 enum DataTypes{
 MY_INT,
 MY_FLOAT,
 MY_STRING,
 MY_BOOL,
-MY_NEDEF
+MY_NEDEF //asta este pentru obiecte din clase cred si pt nume de functii, simboluri fara un tip definit
 };
-
+//urmeaza o clasa ajutatoare pentru a retine valoarea fiecarei variabile
 class Val{
   public:
-    DataTypes tip;
-    int ival = 0;
+    DataTypes tip; //poate fi orice tip de mai sus din enum
+    int ival = 0; //aici initializez orice variabila cu ceva standard (0) ca sa nu mi le initializeze cu ceva valoare random gen 989897
     float fval = 0.0;
     string sval = "";
     bool bval = true;
-
+  //constructorii astia ar trebui sa ne faca viata mai usoara cand vom implementa AST:
     Val() : tip(MY_NEDEF) {} 
     Val(int v) : tip(MY_INT), ival(v) {}
     Val(float v) : tip(MY_FLOAT), fval(v) {}
@@ -31,34 +32,34 @@ class Val{
 
 
 };
-
+//alta clasa ajutatoare. pentru fiecare id voi avea un obiect de tipul IdInfo unde retin tipul, numele, kind(clasa,functie,variabila) si valoarea id-ului;
 class IdInfo {
 public:
     string type;
     string name;
     string kind;
     Val value;
-    vector<string> paramTypes;
+    vector<string> paramTypes; // acest camp din clasa va fi folosit numai daca kind=functie si ii retinem si parametrii
     IdInfo() {}
     IdInfo(string type, string name, string kind) 
         : type(type), name(name), kind(kind) {}
 };
-
+//clasa principala:
 class SymTable {
-    SymTable* pred;          
-    map<string, IdInfo> ids;    
-    string scopeName;          
+    SymTable* pred; //am un pointer la parintele scopului meu. adica daca sunt intr-o functie dintr-o clasa, pred va fi clasa, iar pentru clasa respectiva, pred va fi scopul global.         
+    map<string, IdInfo> ids;    //pt numele fiecarui id, tin toate informatiile despre acesta
+    string scopeName;    //scopul in care sunt      
 
 public:
-    SymTable(string name, SymTable* pred = NULL);
+    SymTable(string name, SymTable* pred = NULL); //pred e default NULL findca scopul global are parintele null
     
-    void addVar(string type, string name, string kind= "variabila");
-    void setVal(string name, Val v);
-    void setParams(string name, vector<string> params);
-    bool existsId(string name);
-    IdInfo* findId(string name);
-    SymTable* getPred();
-    void print(ofstream& file);
+    void addVar(string type, string name, string kind= "variabila"); //adaug id-uri in tabela de simboluri
+    void setVal(string name, Val v); //setez valoarea pt variabile
+    void setParams(string name, vector<string> params); //asta e doar pt functii ca sa le setez parametrii
+    bool existsId(string name); //verific ca exista un id atunci cand acesta este folosit
+    IdInfo* findId(string name); //il gaseste
+    SymTable* getPred();//functie care ma ajuta sa merg si in scope-urile parinte ca sa caut variabile
+    void print(ofstream& file); //e pentru a printa tabele de simboluri(Se cere in enuntul temei)
 
     ~SymTable();
 };
