c1335d374a0dcddf8a5aa8c88a2edebfccceb0de
Darie Iulia <iuliadarie25@gmail.com>
2025-12-28 01:02:08 +0200

    Update SymTableHelp.h

--- tema/SymTableHelp.h
+++ tema/SymTableHelp.h
@@ -1,14 +1,20 @@
+#pragma once
 #include "SymTable.h"
+
+//scopul acestei clase este sa lucram direct cu ea in fisierul bison fiindca profa vrea sa avem cat mai putin cod scris in c++ in fisierul limbaj.y
+// aici cumva manevram un obiect de tipul SymTable si setam totul. functiile si variabilele sunt statice pentru a putea lucra cu ele direct in limbaj.y, 
+//fara sa mai trebuiasca sa declaram un obiect de tipul SymTableHelp
+
 class SymTableHelp{
     private:
  
-         static SymTable* currentScope; 
-         static ofstream fisier;
-         static string currentType; 
-    
+         static SymTable* currentScope; //scopul in care ma aflu
+         static ofstream fisier; //fisierul o sa fie tables.txt, cum cere in tema, pentru a printa pe el tabelele de simboluri
+         static string currentType; //tipul id-ului abia citit
+         static vector<string> paramBuffer; //aici voi tine minte parametrii de functii
+         static string currentFuncName; //din nou, pentru functii
     public:
-    static vector<string> paramBuffer; 
-    static string currentFuncName;
+        //astea o sa le explic in .cpp, desi au nume sugestive
         static void Init();          
         static  void Clean();       
         static void EnterScope(string name);
